d18e2a418edbea5b25d1fcacc0be0f9b87a5975b
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2023-04-17 15:53:33 +0200

    Maj ADRAR

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,41 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
 
 #[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(ChartBuilderInterface $chartBuilder): Response
+    public function index(): Response
     {
-        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
-
-        $chart->setData([
-            'labels' => ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            'datasets' => [
-                [
-                    'label' => 'My First dataset',
-                    'backgroundColor' => 'rgb(255, 99, 132)',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 10, 5, 2, 20, 30, 45],
-                ],
-            ],
-        ]);
-
-        $chart->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 100,
-                ],
-            ],
-        ]);
+        $myChart = array(
+            'type' => 'bar',
+            'data' => array(
+                'labels' => ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                'datasets' => array(
+                    array(
+                        'label' => '# of Votes',
+                        'data' => [12, 19, 3, 5, 2, 3],
+                        'borderWidth' => 1
+                    )
+                )
+            ),
+            'options' => array(
+                'scales' => array(
+                    'y' => array(
+                        'beginAtZero' => true
+                    )
+                )
+            )
+        );
 
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-            'chart' => $chart,
+            'myChart' => $myChart,
         ]);
     }
 }
